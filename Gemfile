148a5bc620cd64ce86df635142ee24ebbb81d49b
Farrah Hashmi <farrahhhashmi@gmail.com>
2017-04-07 18:29:29 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,43 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 5.0.1'
+
+gem 'pg', '~> 0.18'
+
+gem 'puma', '~> 3.0'
+
+gem 'sass-rails', '~> 5.0'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.2'
+
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.5'
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'sendgrid-rails', '~> 2.0', '>= 2.0.5'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'ransack'
+
+gem 'hirb'
+gem 'bootstrap-sass'
+gem 'devise'
+gem 'toastr-rails'
+gem 'hirb'
+gem 'font-awesome-sass'
+gem 'paperclip'
+gem 'geocoder'
---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+  
+  root 'pages#home'
+
+  devise_for 	:users
+            # :path => '',
+            # :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
+            # :controllers => {:omniauth_callbacks => 'omniauth_callbacks',
+            #                  :registrations => 'registrations'
+            #                 }
+
+  resources :users, only: [:show]
+  resources :rooms
+  resources :photos
+
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
+
+  resources :rooms do
+    resources :reviews, only: [:create, :destroy]
+  end
+
+  get '/preload' => 'reservations#preload'
+  get '/preview' => 'reservations#preview'
+
+  get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
+
+  get '/search' => 'pages#search'
+end
